ef68b39095b6d62685bfcdfd40e600fdad599aab
dangnam739 <kieudangnam99@gmail.com>
2020-11-30 08:07:02 +0000

    add config in aws and ruby version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "f3d9593013aa4a5f8724e2e429d9b6a0.vfs.cloud9.us-east-1.amazonaws.com"
 end
